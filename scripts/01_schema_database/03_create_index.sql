55e16873c14c51b6e5fcb621d679cbe78d2d6e3e
eadarak00 <eadarak2001@gmail.com>
2025-08-30 17:48:22 +0000

    feat: creation des index

---
+++ scripts/01_schema_database/03_create_index.sql
@@ -0,0 +1,5 @@
+-- Index pour cluster_nom_prenom
+CREATE INDEX idx_cluster_nom_prenom ON CLUSTER cluster_nom_prenom;
+
+-- Index pour cluster_code
+CREATE INDEX idx_cluster_code ON CLUSTER cluster_code;
