b3c23f73acee09a6e9a68244a1dcfd94a23c0c3e
eadarak00 <eadarak2001@gmail.com>
2025-08-30 17:46:40 +0000

    fix: modification des clusters

--- scripts/01_schema_database/02_create_clusters.sql
+++ scripts/01_schema_database/02_create_clusters.sql
@@ -1,5 +1,15 @@
+DROP CLUSTER cluster_nom_prenom INCLUDING TABLES;
+DROP CLUSTER cluster_code INCLUDING TABLES;
+
 -- Cluster basé sur (Nom, Prenom)
-CREATE CLUSTER cluster_nom_prenom (Nom VARCHAR2(50), Prenom VARCHAR2(50));
+CREATE  CLUSTER cluster_nom_prenom (
+    Nom    VARCHAR2(50),
+    Prenom VARCHAR2(50)
+)
+SIZE 1024;
 
 -- Cluster basé sur Code
-CREATE CLUSTER cluster_code (Code VARCHAR2(10));
+CREATE CLUSTER cluster_code (
+    Code VARCHAR2(10)
+)
+SIZE 1024;
