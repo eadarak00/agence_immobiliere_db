75c6064d8e135f324d0eff19a0617f2b85aef2f1
eadarak00 <eadarak2001@gmail.com>
2025-08-30 16:28:17 +0000

    feat: Creation des clusters

---
+++ scripts/01_schema_database/02_create_clusters.sql
@@ -0,0 +1,5 @@
+-- Cluster basé sur (Nom, Prenom)
+CREATE CLUSTER cluster_nom_prenom (Nom VARCHAR2(50), Prenom VARCHAR2(50));
+
+-- Cluster basé sur Code
+CREATE CLUSTER cluster_code (Code VARCHAR2(10));
