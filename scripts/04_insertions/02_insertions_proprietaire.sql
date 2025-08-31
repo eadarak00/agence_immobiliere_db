9513b4a91916031b8d0a24e0ddaf2f5855e86d56
eadarak00 <eadarak2001@gmail.com>
2025-08-31 14:36:07 +0000

    feat: Script d'insertion des proprietaires

---
+++ scripts/04_insertions/02_insertions_proprietaire.sql
@@ -0,0 +1,7 @@
+DELETE FROM Proprietaire;
+-- Insertion des propriétaires (5 propriétaires)
+INSERT INTO Proprietaire VALUES ('1377192300145', 'DIOP', 'Mamadou', 'Medina, Dakar', 45, 'M', '77-111-2222');
+INSERT INTO Proprietaire VALUES ('1288051200234', 'FALL', 'Fatou', 'Plateau, Dakar', 38, 'F', '78-222-3333');
+INSERT INTO Proprietaire VALUES ('1355032100123', 'NDIAYE', 'Ousmane', 'Grand Yoff, Dakar', 52, 'M', '70-333-4444');
+INSERT INTO Proprietaire VALUES ('1299122500098', 'SARR', 'Aissatou', 'Sacre Coeur, Dakar', 41, 'F', '76-444-5555');
+INSERT INTO Proprietaire VALUES ('1366081800156', 'BA', 'Ibrahima', 'HLM, Dakar', 35, 'M', '77-555-6666');
