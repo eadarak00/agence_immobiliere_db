cd3da1987679a470506f95a7cf1a1971bad6f690
eadarak00 <eadarak2001@gmail.com>
2025-08-31 12:47:06 +0000

    feat: Creation des sequence pour les locataires et les appartements

---
+++ scripts/02_sequences/01_sequences_creation.sql
@@ -0,0 +1,13 @@
+-- Séquence pour Locataire
+CREATE SEQUENCE seq_locataire
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
+
+-- Séquence pour Appartement
+CREATE SEQUENCE seq_appartement
+  START WITH 1
+  INCREMENT BY 1
+  NOCACHE
+  NOCYCLE;
