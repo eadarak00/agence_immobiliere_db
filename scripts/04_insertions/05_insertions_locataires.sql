af1ba4f139b43ad169bd58b769bf8677b39dc7d1
eadarak00 <eadarak2001@gmail.com>
2025-08-31 14:37:14 +0000

    feat: Script d'insertion des locataires

---
+++ scripts/04_insertions/05_insertions_locataires.sql
@@ -0,0 +1,10 @@
+-- Insertion des locataires (8 locataires) avec séquence
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('THIAM', 'Aminata', 'Parcelles Assainies, Dakar', 28, 'F', '77-777-8888', 'Enseignante');
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('GUEYE', 'Moussa', 'Pikine, Dakar', 34, 'M', '78-888-9999', 'Ingenieur');
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('DIOUF', 'Marieme', 'Rufisque', 31, 'F', '70-999-0000', 'Commercante');
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('CAMARA', 'Seydou', 'Guediawaye', 42, 'M', '76-000-1111', 'Chauffeur');
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('SOW', 'Mariama', 'Thies', 26, 'F', '77-111-2222', 'Infirmiere');
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('DIALLO', 'Amadou', 'Mbour', 39, 'M', '78-222-3333', 'Comptable');
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('KANE', 'Khady', 'Saint-Louis', 33, 'F', '70-333-4444', 'Avocate');
+INSERT INTO Locataire(Nom, Prenom, Adresse, Age, Sexe, Telephone, Fonction) VALUES ('MBAYE', 'Cheikh', 'Kaolack', 29, 'M', '76-444-5555', 'Medecin');
+
