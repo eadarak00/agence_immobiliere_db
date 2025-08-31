253239721a4615cc7bd88dc677ad26b62f885d60
eadarak00 <eadarak2001@gmail.com>
2025-08-31 23:37:28 +0000

    feat : job pour le fonction

---
+++ scripts/05_jobs/01_job_prix_mensuel.sql
@@ -0,0 +1,15 @@
+SET SERVEROUTPUT ON;
+
+BEGIN
+  DBMS_SCHEDULER.CREATE_JOB (
+    job_name        => 'job_prix_mensuel',
+    job_type        => 'PLSQL_BLOCK',
+    job_action      => 'DECLARE v_prix NUMBER; BEGIN v_prix := pkg_agence.prix_mensuel(''IMM001''); END;',
+    start_date      => SYSTIMESTAMP,
+    -- repeat_interval => 'FREQ=MINUTELY; INTERVAL=10',  -- toutes les 10 minutes
+    repeat_interval => 'FREQ=SECONDLY; INTERVAL=10',
+    end_date        => NULL,
+    enabled         => TRUE
+  );
+END;
+/
