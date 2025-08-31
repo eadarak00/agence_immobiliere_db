022044c4e3609c5a4e78359681c86fee41f87641
eadarak00 <eadarak2001@gmail.com>
2025-08-31 14:36:36 +0000

    feat: Script d'insertion des immeubles

---
+++ scripts/04_insertions/03_insertions_immeubles.sql
@@ -0,0 +1,8 @@
+-- Insertion des immeubles (7 immeubles)
+INSERT INTO Immeuble VALUES ('IMM001', 'Rue 15 x 16 Medina, Dakar', 'Residentiel', TO_DATE('15-03-2018', 'DD-MM-YYYY'), 4);
+INSERT INTO Immeuble VALUES ('IMM002', 'Avenue Pompidou, Plateau', 'Commercial', TO_DATE('22-07-2019', 'DD-MM-YYYY'), 6);
+INSERT INTO Immeuble VALUES ('IMM003', 'Cite Keur Gorgui', 'Residentiel', TO_DATE('10-11-2017', 'DD-MM-YYYY'), 3);
+INSERT INTO Immeuble VALUES ('IMM004', 'Route de Ngor, Almadies', 'Mixte', TO_DATE('05-09-2020', 'DD-MM-YYYY'), 5);
+INSERT INTO Immeuble VALUES ('IMM005', 'Rue 12 Point E', 'Residentiel', TO_DATE('18-01-2019', 'DD-MM-YYYY'), 4);
+INSERT INTO Immeuble VALUES ('IMM006', 'Boulevard du Centenaire', 'Commercial', TO_DATE('30-05-2021', 'DD-MM-YYYY'), 7);
+INSERT INTO Immeuble VALUES ('IMM007', 'Liberte 6 Extension', 'Residentiel', TO_DATE('12-12-2018', 'DD-MM-YYYY'), 3);
